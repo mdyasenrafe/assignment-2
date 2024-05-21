a3fef0f68097d40a1ee495ca7a90cb777613725f
Md yasen rafe <mdyasenrafe@gmail.com>
2024-05-22 00:36:15 +0600

    strong types added

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,29 +1,38 @@
 import { IProduct } from "./product.interface";
 import ProductModel from "./product.model";
 
-const createProductIntoDB = async (productData: IProduct) => {
+const createProductIntoDB = async (
+  productData: IProduct
+): Promise<IProduct> => {
   const product = new ProductModel(productData);
   return await product.save();
 };
-const getAllProductsFromDB = async () => {
+
+const getAllProductsFromDB = async (): Promise<IProduct[]> => {
   return await ProductModel.find({});
 };
 
-const getProductByIdFromDB = async (productId: string) => {
+const getProductByIdFromDB = async (
+  productId: string
+): Promise<IProduct | null> => {
   return await ProductModel.findById(productId);
 };
 
-const updateProductByFromDB = async (productId: string, bodyData: IProduct) => {
-  const query = { _id: productId };
-  const update = bodyData;
+const updateProductByFromDB = async (
+  productId: string,
+  bodyData: IProduct
+): Promise<IProduct | null> => {
   const options = { new: true };
-  return await ProductModel.findOneAndUpdate(query, update, options);
+  return await ProductModel.findByIdAndUpdate(productId, bodyData, options);
 };
-const deleteProductByFromDB = async (productId: string) => {
+
+const deleteProductByFromDB = async (
+  productId: string
+): Promise<IProduct | null> => {
   return await ProductModel.findByIdAndDelete(productId);
 };
 
-const searchProductsInDB = async (searchTerm: string) => {
+const searchProductsInDB = async (searchTerm: string): Promise<IProduct[]> => {
   const regex = new RegExp(searchTerm, "i");
   return await ProductModel.find({
     $or: [
